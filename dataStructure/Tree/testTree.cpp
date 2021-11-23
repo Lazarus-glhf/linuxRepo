e89684cecc1d034c3ee26ae8141374279c3e1da7
Lazarus0416 <redrumlivedog@outlook.com>
2021-11-23 20:44:12 +0800

    add tree

---
+++ dataStructure/Tree/testTree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Node
+{
+
+
+	T item;
+	Node* lChild;
+	Node* rChild;
+
+public:
+	Node(T i):item(i), lChild(nullptr), rChild(nullptr)
+	{
+		
+	}
+
+	void creat()
+	{
+		T i;
+		cin >> i;
+		
+	}
+
+
+
+
+
+	
+
+
+};
